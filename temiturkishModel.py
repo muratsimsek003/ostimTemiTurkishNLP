79aa9d96bde3b7271fd6cc12453b5b3cc596937d
murat <muratsimsek@isteknoloji.com.tr>
2024-03-12 11:37:47 +0300

    version3

--- temiturkishModel.py
+++ temiturkishModel.py
@@ -4,18 +4,22 @@ from texttoSpeech import texttoSpeech
 from speechtoText import speechtoText
 import time
 
+def main():
+    # LOAD MODEL
+    tokenizer = AutoTokenizer.from_pretrained("muratsimsek003/turkish_bert_qa")
+    model = AutoModelForQuestionAnswering.from_pretrained("muratsimsek003/turkish_bert_qa")
+    qa=pipeline("question-answering", model=model, tokenizer=tokenizer)
 
-# LOAD MODEL
-tokenizer = AutoTokenizer.from_pretrained("muratsimsek003/turkish_bert_qa")
-model = AutoModelForQuestionAnswering.from_pretrained("muratsimsek003/turkish_bert_qa")
-qa=pipeline("question-answering", model=model, tokenizer=tokenizer)
-
-with open('ostim.txt', encoding='utf-8') as dosya:
-    ostim = dosya.read()
+    with open('ostim.txt', encoding='utf-8') as dosya:
+        ostim = dosya.read()
 
 
-os_question=speechtoText()
+    os_question=speechtoText()
 
-cevap = qa(question=os_question, context=ostim)["answer"]
-print(cevap)
-texttoSpeech(cevap)
+    cevap = qa(question=os_question, context=ostim)["answer"]
+    print(cevap)
+    texttoSpeech(cevap)
+
+
+if __name__ == "__main__":
+    main()
